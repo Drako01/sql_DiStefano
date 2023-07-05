0bc1263839ecf3ef67fde1480fa2469be0fe1d27
Alejandro Di Stefano <addistefano@live.com.ar>
2023-07-05 18:50:00 -0300

    BackUp

--- backup.sql
+++ backup.sql
@@ -1,3 +1,28 @@
+-- Para generar el backup de la base de datos clinica, usar el siguiente comando:
+
+-- Comando: mysqldump -u root -p [clinica] --no-create-info clinica > backup.sql
+-- Base de datos: clinica 
+-- Archivo: backup.sql
+-- Para restaurar la base de datos clinica, usar el siguiente comando:
+-- Comando: mysqldump -u root -p  [clinica] < backup.sql
+
+-- Tablas incluidas en la base de datos "clinica":
+-- - especialidadmedica
+-- - localidad
+-- - medico
+-- - consultorios
+-- - obrasocial
+-- - persona
+-- - turno
+-- - log_medico
+-- - log_turno
+-- - empleado
+-- - vacaciones
+-- - vacaciones_medico
+
+-----------------------------------------------------------------------------------------------------------------------------------
+
+
 -- MariaDB dump 10.19  Distrib 10.4.28-MariaDB, for Win64 (AMD64)
 --
 -- Host: localhost    Database: clinica
